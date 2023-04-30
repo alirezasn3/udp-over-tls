cefca8e6bfaca4d793043eb33dc0ca8d69959f55
Alireza Saadatnia <alirezasn3@gmail.com>
2023-04-30 09:20:18 +0330

    updated

--- main.go
+++ main.go
@@ -39,9 +39,16 @@ func loadCertificates(config *Config) {
 	if err != nil {
 		panic(err)
 	}
-	config.TLSConfig.MinVersion = tls.VersionTLS13
+	config.TLSConfig.MinVersion = tls.VersionTLS12
 	config.TLSConfig.Certificates = []tls.Certificate{certificate}
 	config.TLSConfig.InsecureSkipVerify = true
+	config.TLSConfig.CurvePreferences = []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256}
+	config.TLSConfig.CipherSuites = []uint16{
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+	}
 }
 
 func main() {
